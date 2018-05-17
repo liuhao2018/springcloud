433af964e2861b080cdbb797e167b709c4c58eab
liuhao <liuhao@limefamily.com>
2018-05-17 17:11:34 +0800

    网关熔断

---
+++ netflix-zuul-gateway/src/main/java/com/liuhao/springcloud/netflix/zuul/gateway/fallback/CustomZuulFallbackProvider.java
@@ -0,0 +1,54 @@
+package com.liuhao.springcloud.netflix.zuul.gateway.fallback;
+
+
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@Component
+public class CustomZuulFallbackProvider implements FallbackProvider{
+    @Override
+    public String getRoute() {
+        return "*";
+    }
+
+    @Override
+    public ClientHttpResponse fallbackResponse(String route, Throwable cause) {
+        return new ClientHttpResponse() {
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return null;
+            }
+
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return 0;
+            }
+
+            @Override
+            public String getStatusText() throws IOException {
+                return null;
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {
+                return null;
+            }
+
+            @Override
+            public HttpHeaders getHeaders() {
+                return null;
+            }
+        };
+    }
+}
